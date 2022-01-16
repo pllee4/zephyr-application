46715db8ee4cd815f042b9ce1bd46f0d091bd576
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-01-16 16:47:39 +0800

    src: app1: app.cmake: added VersionEnquiryManager

--- src/app/app1/app.cmake
+++ src/app/app1/app.cmake
@@ -6,3 +6,8 @@ configure_file(version/app_version.h.in ${APP_NAME}/include/app_version.h)
 target_include_directories(app PRIVATE ${CMAKE_BINARY_DIR}/src/app/${APP_NAME}/include)
 
 target_sources(app PRIVATE ${APP_NAME}/main.c)
+target_link_libraries(app PRIVATE VersionEnquiryManager)
+target_compile_definitions(app PRIVATE
+    ENQUIRY_MGR_VER_MIN=$<TARGET_PROPERTY:ENQUIRY_MGR_VER_MIN>
+    ENQUIRY_MGR_VER_MAX=$<TARGET_PROPERTY:ENQUIRY_MGR_VER_MAX>
+)
